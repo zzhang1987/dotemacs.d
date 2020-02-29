4f7e61607c720f32fed65445a11f655afc035573
zzhang <zhen@zzhang.org>
2020-02-29 18:03:21 +0800

    updated

--- init.el
+++ init.el
@@ -43,6 +43,15 @@
   :ensure t)
 (auto-package-update-maybe)
 
+(if (eq system-type 'darwin)
+    (progn
+      (getenv "PATH")
+      (setenv "PATH"
+              (concat
+               "/Library/TeX/texbin/" ":"
+               (getenv "PATH")))
+      )
+    )
 (add-to-list 'exec-path "/home/zzhang/miniconda3/bin/")
 
 ;; Auto compiling elc file
@@ -106,3 +115,15 @@
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
