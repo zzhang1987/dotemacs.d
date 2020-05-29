c834d3e648ceb5c73af984f2ec41d617ee277128
zzhang <zhen@zzhang.org>
2020-05-29 10:45:37 +0800

    updated

--- init.el
+++ init.el
@@ -116,3 +116,17 @@
 (setq exec-path-from-shell-arguments '("-l"))
 (exec-path-from-shell-initialize)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck pyvenv yasnippet xclip vimish-fold undo-tree swiper-helm sphinx-doc solarized-theme req-package realgud rainbow-identifiers rainbow-delimiters python-docstring py-autopep8 project-explorer powerline pdf-tools ob-ipython nyan-mode nginx-mode neotree modern-cpp-font-lock magit magic-latex-buffer lsp-ui lsp-java helm-projectile helm-company helm-bibtex haskell-mode gscholar-bibtex gradle-mode go-mode git-gutter+ find-file-in-project fill-column-indicator exec-path-from-shell el-get dashboard dap-mode company-lsp company-box cmake-project cmake-mode better-shell better-defaults auto-package-update auto-org-md auto-compile auctex-latexmk all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
