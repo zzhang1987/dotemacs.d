eea352d367aa6e4d2c2124a326a054794b54153b
Zhen Zhang <zhen@zzhang.org>
2022-08-25 11:31:50 +0930

    updated custom vars

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(flycheck zenburn-theme yasnippet yapfify xclip vimish-fold undo-tree tabbar swiper-helm sphinx-doc req-package realgud rainbow-identifiers rainbow-delimiters pyvenv python-docstring project-explorer powerline pdf-tools ob-ipython nyan-mode nginx-mode neotree modern-cpp-font-lock material-theme magit lsp-java helm-projectile helm-company helm-bibtex haskell-mode gradle-mode go-mode git-gutter+ find-file-in-project fill-column-indicator dashboard cmake-project cmake-mode better-shell better-defaults auto-package-update auto-org-md auto-compile auctex all-the-icons)
+
+ '(markdown-command "/usr/bin/pandoc --mathjax")
+ '(markdown-display-remote-images t)
+ '(markdown-enable-math t)
+
+   )
+ '(tabbar-separator '(0.5)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
