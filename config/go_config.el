4e96e73534743990d0cd07e0afb781f95e4774b1
Zhen Zhang <zhen@zzhang.org>
2024-05-07 10:49:56 +0930

    add go-mode

--- config/go_config.el
+++ config/go_config.el
@@ -1,7 +1,7 @@
 (provide 'go_config)
 
 (use-package go-mode
-  :ensure t
+  :straight t
   :config
   (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
   (add-hook 'go-mode-hook 'lsp-deferred)
